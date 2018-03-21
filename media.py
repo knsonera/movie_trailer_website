07ec36ddd0d1597286cc2113288dcf143f1f2dd2
knsonera <37396326+knsonera@users.noreply.github.com>
2018-03-21 16:33:01 -0700

    added comments to Movie class

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
+# Movie class is the main data structure for Movie Trailer website.
+# To create an instance (a movie), call the class with three parameters:
+# - movie title
+# - poster image url
+# - youtube video url
+# Example:
+# the_movie = Movie("The Movie", "http://img.url.com", "youtube-video.url.com")
+
+
 class Movie:
     def __init__(self, title, poster_url, trailer_url):
         self.title = title
         self.poster_image_url = poster_url
         self.trailer_youtube_url = trailer_url
-
